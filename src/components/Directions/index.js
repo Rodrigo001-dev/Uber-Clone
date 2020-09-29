a50573370252685b27969518ef2a49898094a546
Rodrigo <rodrigorael53@gmail.com>
2020-09-29 00:21:33 -0300

    Clone

---
+++ src/components/Directions/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import MapViewDirections from 'react-native-maps-directions';
+
+const Directions = ({ destination, origin, onReady }) => (
+  <MapViewDirections 
+    destination={destination}
+    origin={origin}
+    onReady={onReady}
+    apikey="AIzaSyB108amubeMkw_7ok2jUhtVj9IkME9k8sc"
+    strokeWidth={3}
+    strokeColor="#222"
+  />
+);
+
+export default Directions;
+
---
+++ src/index.js
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable eol-last */
+/* eslint-disable semi */
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import Map from './components/Map';
+
+const App = () => <Map /> 
+
+export default App;
